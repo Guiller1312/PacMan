ff1259704f51ea15e61298deaff2b79c4dd73d4c
Guillermo Felipe <guillermodf76@gmail.com>
2020-02-10 20:14:59 +0100

    PacMan_principio

---
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject enemigo, coleccionable;
+    public Vector3 posicion;
+    public int numeroEnemigos;
+    public float esperaInicial;
+    public float esperaEntreEnemigos;
+    public float esperaEntreOlas, esperaEntreColeccionables;
+	//array de posiciones donde apareceran los enemigos
+	public float [] posEx = {-4.1f, 3.8f, 6.85f, 1.6f, -7.23f, -8.8f, 9.1f};
+	public float [] posEz = {-7.3f, -3.9f, -5.9f, 5.4f, 5.4f, -3, 0.86f};
+
+	void Start() {  
+
+        //LLamo a la rutina de crear enemigos
+        StartCoroutine(crearEnemigos());
+
+        //LLamo a la rutina de crear coleccionables
+        StartCoroutine(crearColeccionables());
+
+    }
+		
+    IEnumerator crearEnemigos()
+    {
+       //Espero un tiempo antes de crear enemigos
+       yield return new WaitForSeconds(esperaInicial);
+
+        //Bucle durante toda la vida del juego
+        while (true)
+        {
+            //Bucle de número de enemigos
+            for (int i = 0; i < numeroEnemigos; i++)
+            {
+			
+                //Instancio el enemigo en una posición aleatoria del tablero
+                //Vector3 posicionEnemigo = new Vector3(Random.Range(-posicion.x, posicion.x), posicion.y, Random.Range(-posicion.z, posicion.z));
+				int indicePos = Random.Range(0,7);
+				Vector3 posicionEnemigo = new Vector3((posEx[indicePos]), posicion.y, (posEz[indicePos]));
+                Quaternion rotacionEnemigo = Quaternion.identity;
+                Instantiate(enemigo, posicionEnemigo, rotacionEnemigo);
+
+                //Espero un tiempo entre la creación de cada enemigo
+                yield return new WaitForSeconds(esperaEntreEnemigos);
+            }
+
+            //Espero un tiempo entre oleadas de enemigos
+            yield return new WaitForSeconds(esperaEntreOlas);
+        }
+    }
+
+    IEnumerator crearColeccionables()
+    {
+        yield return new WaitForSeconds(esperaInicial);
+        while (true)
+        {
+            //Instancio el coleccionable en una posición aleatoria del tablero
+            Vector3 posicionColeccionable = new Vector3(Random.Range(-posicion.x, posicion.x), posicion.y, Random.Range(-posicion.z, posicion.z));
+            Quaternion rotacionColeccionable = Quaternion.identity;
+            Instantiate(coleccionable, posicionColeccionable, rotacionColeccionable);
+
+            //Espero un tiempo entre la creación de cada coleccionable
+            yield return new WaitForSeconds(esperaEntreColeccionables);
+               
+
+        }
+
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/Jugador.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+
+    public float velocidad = 0.1f;
+	
+	void FixedUpdate () {
+
+        //Capturo el movimiento en los ejes
+        float movimientoH = Input.GetAxis("Horizontal");
+        float movimientoV = Input.GetAxis("Vertical");
+
+        //Genero el vector de movimiento
+        Vector3 movimiento = new Vector3(movimientoH, 0, movimientoV);
+
+        //Muevo el jugador
+        transform.position += -movimiento * velocidad;
+		
+	}
+}
